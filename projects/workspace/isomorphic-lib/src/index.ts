7e36162735802a5f1a7bfdfcb14df152ad743a8c
Dariusz <dariusz@Mac-Pro.local>
2018-01-21 03:09:35 +0100

    optymalizations

--- projects/workspace/isomorphic-lib/src/index.ts
+++ projects/workspace/isomorphic-lib/src/index.ts
@@ -1,66 +1,71 @@
 
 //#region controler
+const controllers = {};
+const entities = {};
+function addController(controller: Function) {
+    controllers[controller.name] = controller;
+}
+function addEntity(entity: Function) {
+    entities[entity.name] = entity;
+}
+
 import { UsersController } from "./controllers/examples/UsersController";
 export { UsersController } from "./controllers/examples/UsersController";
+addController(UsersController)
 
 import { HelloController } from "./controllers/examples/HelloController";
 export { HelloController } from "./controllers/examples/HelloController";
+addController(HelloController)
 
 import { TestController } from "./controllers/examples/TestController";
 export { TestController } from "./controllers/examples/TestController";
+addController(TestController)
 
 import { ParentClass } from './controllers/examples/ParentControllers';
 export { ParentClass } from './controllers/examples/ParentControllers';
+addController(ParentClass)
 
 import { ChildClass } from './controllers/examples/Child1Controller';
 export { ChildClass } from './controllers/examples/Child1Controller';
+addController(ChildClass)
 
 import { ChildClass2 } from './controllers/examples/Child2Controller';
 export { ChildClass2 } from './controllers/examples/Child2Controller';
+addController(ChildClass2)
 
 import { AuthController } from './controllers/AuthController';
 export { AuthController } from './controllers/AuthController';
-
-export const Controllers = {
-    UsersController,
-    HelloController,
-    TestController,
-    ParentClass,
-    ChildClass,
-    ChildClass2,
-    AuthController
-};
-
+addController(AuthController)
 
 
 import { User } from "./entities/examples/User";
 export { User } from "./entities/examples/User";
+addEntity(User)
 
 import { Book } from "./entities/examples/Book";
 export { Book } from "./entities/examples/Book";
+addEntity(Book)
 
 import { Author } from "./entities/examples/Author";
 export { Author } from "./entities/examples/Author";
+addEntity(Author)
 
 import { EMAIL_TYPE } from "./entities/EMAIL_TYPE";
 export { EMAIL_TYPE } from "./entities/EMAIL_TYPE";
+addEntity(EMAIL_TYPE)
 
 import { EMAIL } from "./entities/EMAIL";
 export { EMAIL } from "./entities/EMAIL";
+addEntity(EMAIL)
 
 import { USER } from "./entities/USER";
 export { USER } from "./entities/USER";
+addEntity(USER)
 
 import { SESSION } from "./entities/SESSION";
 export { SESSION } from "./entities/SESSION";
+addEntity(SESSION)
 
 
-export const Entities = {
-    User,
-    Book,
-    Author,
-    EMAIL_TYPE,
-    EMAIL,
-    USER,
-    SESSION
-};
+export const Controllers = controllers;
+export const Entities = entities;
