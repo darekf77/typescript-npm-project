429ba8365cb66bc9b740fb31e43f6763fd669600
Dariusz <dariusz@Mac-Pro.local>
2018-01-26 01:51:38 +0100

    a

--- projects/workspace/isomorphic-lib/src/controllers/AuthController.ts
+++ projects/workspace/isomorphic-lib/src/controllers/AuthController.ts
@@ -140,7 +140,7 @@ export class AuthController {
             const repo = await self.__repos();
             let User: USER = req.user;
             if (!User) {
-                throw new Error('No user for logout');
+                throw 'No user for logout';
             }
             User = await USER.byId(User.id, repo.user);
             if (!User) {
