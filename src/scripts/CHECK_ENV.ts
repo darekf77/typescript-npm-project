cc3e5de542bcd886e311b9129687dcdd8d00746d
Dariusz <dariusz@Mac-Pro.local>
2018-03-07 21:42:35 +0100

    check global dependencies

--- src/scripts/CHECK_ENV.ts
+++ src/scripts/CHECK_ENV.ts
@@ -2,6 +2,12 @@ import { Helpers } from "morphi";
 
 
 export default {
-    $CHECK_ENV: (args) => Helpers.checkEnvironment(),
-    $CHECK_ENVIRONMENT: (args) => Helpers.checkEnvironment(),
+    $CHECK_ENV: (args) => {
+        Helpers.checkEnvironment()
+        process.exit(0)
+    },
+    $CHECK_ENVIRONMENT: (args) => {
+        Helpers.checkEnvironment()
+        process.exit(0)
+    },
 };
