d633e60b1fc7fd5fbf4364bffd679572dba96681
Dariusz <dariusz@Mac-Pro.local>
2018-03-13 21:41:34 +0100

    smooth ok

--- projects/baseline/ss-common-ui/components/src/sliders/slider-vertical/goto-child.directive.ts
+++ projects/baseline/ss-common-ui/components/src/sliders/slider-vertical/goto-child.directive.ts
@@ -1,6 +1,6 @@
 
 
-import { Directive, Input, HostListener } from '@angular/core';
+import { Directive, Input, OnInit, HostListener, ElementRef, AfterContentInit } from '@angular/core';
 
 
 import { Log, Level } from 'ng2-logger';
@@ -8,9 +8,12 @@ import { SliderVerticalComponent } from './slider-vertical.component';
 const log = Log.create('go to vertival slider child', Level.__NOTHING)
 
 @Directive({ selector: '[gotoChildVerticalSlider]' })
-export class GoToChildVerticalSliderDirective {
-  constructor() { }
+export class GoToChildVerticalSliderDirective implements AfterContentInit {
+  constructor(private e: ElementRef) { }
 
+  get element(): HTMLElement {
+    return this.e.nativeElement ? this.e.nativeElement : undefined;
+  }
 
   private childHeader: string
   @Input() set gotoChildVerticalSlider(childHeader: string) {
@@ -18,13 +21,35 @@ export class GoToChildVerticalSliderDirective {
     this.childHeader = childHeader;
   }
 
+  ngAfterContentInit() {
+    // if (!this.parent) {
+    //   this.parent = this.findParent(this.element);
+    // }
+  }
+
   @Input() parent: SliderVerticalComponent;
 
   @HostListener('click')
   click() {
     log.i('goto', this.childHeader)
-    log.i('goto', this)
-    this.parent.goTo(this.parent.getChildBy(this.childHeader))
+    log.i('this.parent', this.parent)
+    if (this.parent) {
+      this.parent.goTo(this.parent.getChildBy(this.childHeader))
+    } else {
+      log.error('no parent element for directive gotoChildVerticalSlider="" ')
+    }
   }
+
+  findParent(elem: HTMLElement): SliderVerticalComponent {
+
+    if (!elem) return;;
+    if (elem instanceof SliderVerticalComponent) {
+      log.i('parent founded!')
+      return elem;
+    }
+    log.d('elem search parent', elem.parentNode)
+    return this.findParent(elem.parentElement);
+  }
+
 
 }
--- projects/baseline/ss-common-ui/components/src/sliders/slider-vertical/index.ts
+++ projects/baseline/ss-common-ui/components/src/sliders/slider-vertical/index.ts
@@ -1,1 +1,4 @@
 export * from './slider-vertical.component';
+export * from './slider-vertical.module';
+export * from './goto-child.directive';
+export * from './child/slider-vertival-child.component'
