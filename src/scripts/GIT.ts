d6eec9bef636a1064acfe18e891189168fb80224
Dariusz <dariusz@cn-n200097.myigt.com>
2018-04-13 13:27:27 +0200

    remove untracked

---
+++ src/scripts/GIT.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Project } from "../project";
+import config from '../config';
+import { paramsFrom } from '../helpers';
+import { run } from '../process';
+
+
+function $GIT_REMOVE_UNTRACKED() {
+    const gitginoredfiles = Project.Current.recreate.filesIgnoredBy.gitignore
+        .filter(f => !(f === config.folder.node_modules)) // link/unlink takes care of node_modules
+    gitginoredfiles.forEach(f => {
+        const p = path.join(Project.Current.location, f);
+        if (fs.existsSync(p)) {
+            if (fs.statSync(p).isDirectory()) {
+                Project.Current.run(`git rm -rf ${f}`).sync()
+            } else {
+                Project.Current.run(`git rm ${f}`).sync()
+            }
+        }
+    });
+    process.exit(0)
+}
+
+export default {
+    $GIT_REMOVE_UNTRACKED,
+    $GIT_REMOVE_UNTRACKED_EVERYWHERE: () => {
+        Project.projects.forEach(p => {
+            run(`tnp ${paramsFrom($GIT_REMOVE_UNTRACKED.name)}`, { cwd: p.location }).sync()
+        })
+        process.exit(0)
+    }
+}
