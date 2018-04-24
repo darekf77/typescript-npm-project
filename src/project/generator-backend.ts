977495a30b36521765ad0470014da0cec3fcf9e2
Dariusz <dariusz@ustx-tc-1005532.myigt.com>
2018-04-24 18:42:18 +0200

    in progress of site entites/controllers partial generator

---
+++ src/project/generator-backend.ts
@@ -0,0 +1,22 @@
+import { Project } from "./base-project";
+
+
+
+
+export class BackendGenerator {
+
+    constructor(private project: Project) {
+
+    }
+
+
+    entities() {
+
+    }
+
+    controllers() {
+
+    }
+
+
+}
