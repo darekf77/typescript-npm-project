a36f85447aeb2f36c5d09c3371093e711bcf2ef2
Dariusz Filipiak <darekf77@gmail.com>
2018-06-05 16:23:57 +0200

    asd

--- projects/baseline/ss-admin-webapp/src/app/+dashboard/course/course-categories/course-categories.component.ts
+++ projects/baseline/ss-admin-webapp/src/app/+dashboard/course/course-categories/course-categories.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-course-categories',
   templateUrl: './course-categories.component.html',
-  styleUrls: ['./course-categories.component.css']
+  styleUrls: ['./course-categories.component.scss']
 })
 export class CourseCategoriesComponent implements OnInit {
 
