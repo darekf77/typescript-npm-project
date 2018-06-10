0e3d824d89a356a16d1f9b90b63d493dacbb5c12
Dariusz <darekf77@gmail.com>
2018-06-10 18:49:53 +0200

    in progress of awesome complex editor

--- projects/baseline/ss-admin-webapp/src/app/+dashboard/+course/course-categories/category-editor/category-editor.module.ts
+++ projects/baseline/ss-admin-webapp/src/app/+dashboard/+course/course-categories/category-editor/category-editor.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
 import { CategoryEditorComponent } from './category-editor.component';
-import { CategoryResolver } from '../model/categorie.resolve';
+
+import { MatListModule } from "@angular/material/list";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+
+const materialModules = [
+  MatListModule,
+  MatTabsModule,
+  MatIconModule,
+  MatInputModule,
+  MatCardModule
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ...materialModules
   ],
   exports: [
     CategoryEditorComponent
   ],
   declarations: [CategoryEditorComponent],
-  providers: [CategoryResolver]
+  providers: []
 })
 export class CategoryEditorModule { }
