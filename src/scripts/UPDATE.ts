9f233b819e2b50faf5ecb5dd4974ad1302300b35
Dariusz Filipiak <darekf77@gmail.com>
2018-10-16 19:55:02 +0200

    asd

--- src/scripts/UPDATE.ts
+++ src/scripts/UPDATE.ts
@@ -6,38 +6,34 @@ import * as path from 'path';
 
 
 
-export function rebuildTnp(pull = false) {
-    const p = Project.Tnp;
-    const backupFolderDist = 'tmp-dist-current';
-    p.run(`rimraf ${backupFolderDist}`).sync();
-    if (pull) {
-        p.run(`git reset --hard`).sync();
-        p.run(`git pull origin master`).sync();
-    }
-    try {
-        p.run(`cpr dist ${backupFolderDist}`).sync()
-        p.run(`(tnp build:dist && rimraf ${backupFolderDist}) || (rimraf dist && cpr ${backupFolderDist} dist  && echo "Something went wrong with rebuild of tnp") `, { output: true }).sync()
-        const backupDist = path.join(p.location, backupFolderDist);
-        if (fs.existsSync(backupDist)) {
-            p.run(`rimraf ${backupFolderDist}`);
-            error(`Tnp self update not successfull`, false, true);
-        } else {
-            info(`Tnp self update success`);
-        }
-    } catch (e) {
+export function rebuildTnp(e) {
+  const p = Project.Tnp;
+  const backupFolderDist = 'tmp-dist-current';
+  p.run(`rimraf ${backupFolderDist}`).sync();
+  p.run(`git reset --hard`).sync();
+  p.run(`git pull origin master`).sync();
 
+  try {
+    p.run(`cpr dist ${backupFolderDist}`).sync()
+    p.run(`(tnp build:dist && rimraf ${backupFolderDist}) || (rimraf dist && cpr ${backupFolderDist} dist  && echo "Something went wrong with rebuild of tnp") `, { output: true }).sync()
+    const backupDist = path.join(p.location, backupFolderDist);
+    if (fs.existsSync(backupDist)) {
+      p.run(`rimraf ${backupFolderDist}`);
+      error(`Tnp self update not successfull`, false, true);
+    } else {
+      info(`Tnp self update success`);
     }
+  } catch (e) {
+
+  }
 }
 
 
 export default {
-    $UPDATE: (args) => {
-        rebuildTnp();
-        process.exit(0)
-    },
-    $UPDATE_PULL: (args) => {
-        rebuildTnp(true);
-        process.exit(0)
-    }
+  $UPDATE: (args) => {
+    rebuildTnp();
+    process.exit(0)
+  }
+
 }
 //#endregion
