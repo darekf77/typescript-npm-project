7f6428fc4a3512a971989cc7d53045d4721e40de
Filipiak <dfilipiak@micros-mbp.mis.gtech.com>
2019-02-13 17:53:39 +0100

    asd

---
+++ configure-mac.sh
@@ -0,0 +1,3 @@
+xcode-select --install # Install Command Line Tools if you haven't already.
+sudo xcode-select --switch /Library/Developer/CommandLineTools # Enable command line tools
+# install XCode and make sure that is in /Application
