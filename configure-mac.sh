02f8b5c7b6db1e258a575e1b3a6e761180c8c21a
Dariusz <dariusz.filipiak@igt.com>
2019-02-14 16:36:11 +0100

    mac

--- configure-mac.sh
+++ configure-mac.sh
@@ -1,3 +1,7 @@
 xcode-select --install # Install Command Line Tools if you haven't already.
 sudo xcode-select --switch /Library/Developer/CommandLineTools # Enable command line tools
 # install XCode and make sure that is in /Application
+nvm alias default node
+nvm install 9.4
+nvm use 9.4
+npm install npm@latest -g
