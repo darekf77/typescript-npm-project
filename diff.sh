612fb04ca1e5801abff381feed69ca55bf30af6c
Dariusz <dariusz.filipiak@igt.com>
2019-04-23 16:33:57 +0200

    asd

--- diff.sh
+++
@@ -1,3 +0,0 @@
-ls -1 /home/darek/projects/npm/tsc-npm-project/node_modules > tmp-tnp.txt
-ls -1 /home/darek/projects/npm/tsc-npm-project/tmp-tests-context/tnp-install---should-install-worksapce-packages/test1/node_modules > tmp-test1.txt
-diff -u  tmp-test1.txt tmp-tnp.txt > tmp-out.txt
