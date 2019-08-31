8f75a64ab28025169f05936ce3489f0c37f47034
Dariusz Filipiak <dariusz.filipiak@igt.com>
2019-08-31 16:31:27 +0200

    inc compi

--- src/scripts/INCREMENTAL-CHANGE-MANAGER.ts
+++ src/scripts/INCREMENTAL-CHANGE-MANAGER.ts
@@ -1,82 +1,88 @@
-// //#region @backend
-// import * as path from 'path';
-// import { CLASS } from 'typescript-class-helpers';
-// import { IncCompiler } from 'incremental-compiler';
-// import { log } from '../helpers/helpers-messages';
+//#region @backend
+import * as path from 'path';
+import { CLASS } from 'typescript-class-helpers';
+import { IncCompiler } from 'incremental-compiler';
+import { info, warn, log, error, runSyncOrAsync, compilationWrapperTnp } from '../helpers';
+
+IncCompiler.init(async (asyncEvents) => {
+
+  const customClientActions = {
+    ClientCompiler1: CLASS.getSingleton<IClientCompiler1>(ClientCompiler1),
+    ClientCompiler2: CLASS.getSingleton<IClientCompiler2>(ClientCompiler2),
+  };
+
+  const c = asyncEvents.clients<typeof customClientActions>(customClientActions);
+
+  // c.ClientCompiler1.asyncAction(asyncEvents, 'This is amaizing');
+
+}, {
+    info,
+    error,
+    warn,
+    log,
+    runSyncOrAsync,
+    compilationWrapper: compilationWrapperTnp
+  });
 
 
-// // IncCompiler.initScenario(async (asyncEvents) => {
+@IncCompiler.Class({ className: 'ClientCompiler1' })
+export class ClientCompiler1 extends IncCompiler.Base<{ dupa: boolean; }> {
 
-// //   const customClientActions = {
-// //     ClientCompiler1: CLASS.getSingleton<IClientCompiler1>(ClientCompiler1),
-// //     ClientCompiler2: CLASS.getSingleton<IClientCompiler2>(ClientCompiler2),
-// //   };
+  async syncAction(files) {
+    // console.log(`sync Files for ${CLASS.getNameFromObject(this)}`, files)
+  }
 
-// //   const c = asyncEvents.clients<typeof customClientActions>(customClientActions);
+  @IncCompiler.methods.AsyncAction()
+  asyncAction(change: IncCompiler.Change, data) {
+    console.log(`Async action ${CLASS.getNameFromObject(this)}, data: ${data}`)
+    return new Promise<any>((resolve) => {
+      // console.log(`async start for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
+      setTimeout(() => {
+        // console.log(`async end for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
+        resolve();
+      }, 2000)
+    })
+  }
 
-// //   // c.ClientCompiler1.asyncAction(asyncEvents, 'This is amaizing');
+}
 
-// // });
+export type IClientCompiler1 = ClientCompiler1;
 
+@IncCompiler.Class({ className: 'ClientCompiler2' })
+export class ClientCompiler2 extends IncCompiler.Base {
+  async syncAction(files) {
+    // console.log(`sync Files for ${CLASS.getNameFromObject(this)}`, files)
+  }
 
-// @IncCompiler.Class({ className: 'ClientCompiler1' })
-// export class ClientCompiler1 extends IncCompiler.Base<{ dupa: boolean; }> {
+  @IncCompiler.methods.AsyncAction()
+  asyncAction(change: IncCompiler.Change, data) {
+    console.log(`Async action ${CLASS.getNameFromObject(this)}, data: ${data}`)
+    return new Promise<any>((resolve) => {
+      // console.log(`async start for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
+      setTimeout(() => {
+        // console.log(`async end for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
+        resolve();
+      }, 2000);
+    });
+  }
+}
+export type IClientCompiler2 = ClientCompiler2;
 
-//   async syncAction(files) {
-//     // console.log(`sync Files for ${CLASS.getNameFromObject(this)}`, files)
-//   }
-
-//   @IncCompiler.methods.AsyncAction()
-//   asyncAction(change: IncCompiler.Change, data) {
-//     console.log(`Async action ${CLASS.getNameFromObject(this)}, data: ${data}`)
-//     return new Promise<any>((resolve) => {
-//       // console.log(`async start for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
-//       setTimeout(() => {
-//         // console.log(`async end for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
-//         resolve();
-//       }, 2000)
-//     })
-//   }
-
-// }
-
-// export type IClientCompiler1 = ClientCompiler1;
-
-// @IncCompiler.Class({ className: 'ClientCompiler2' })
-// export class ClientCompiler2 extends IncCompiler.Base {
-//   async syncAction(files) {
-//     // console.log(`sync Files for ${CLASS.getNameFromObject(this)}`, files)
-//   }
-
-//   @IncCompiler.methods.AsyncAction()
-//   asyncAction(change: IncCompiler.Change, data) {
-//     console.log(`Async action ${CLASS.getNameFromObject(this)}, data: ${data}`)
-//     return new Promise<any>((resolve) => {
-//       // console.log(`async start for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
-//       setTimeout(() => {
-//         // console.log(`async end for ${CLASS.getNameFromObject(this)}`, change.fileAbsolutePath)
-//         resolve();
-//       }, 2000);
-//     });
-//   }
-// }
-// export type IClientCompiler2 = ClientCompiler2;
-
-// export default {
+export default {
 
 
-//   async COMPILER() {
-//     const c1 = await CLASS
-//       .getSingleton<ClientCompiler1>(ClientCompiler1)
-//       .set({ folderPath: path.join(process.cwd(), 'tmp-test1') })
-//       .startAndWatch();
+  async COMPILER() {
+    const c1 = await CLASS
+      .getSingleton<ClientCompiler1>(ClientCompiler1)
+      .set({ folderPath: path.join(process.cwd(), 'tmp-test1') })
+      .startAndWatch();
 
-//     const c2 = await CLASS
-//       .getSingleton<IClientCompiler2>(ClientCompiler2)
-//       .set({ folderPath: path.join(process.cwd(), 'tmp-test1'), executeOutsideScenario: false })
-//       .startAndWatch();
-//   }
+    const c2 = await CLASS
+      .getSingleton<IClientCompiler2>(ClientCompiler2)
+      .set({ folderPath: path.join(process.cwd(), 'tmp-test1'), executeOutsideScenario: false })
+      .startAndWatch();
+  }
 
-// }
+}
 
-// //#endregion
+//#endregion
