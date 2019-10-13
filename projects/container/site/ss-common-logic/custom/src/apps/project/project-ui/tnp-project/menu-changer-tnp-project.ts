f800fe030e5602def9bc48749746f571709771fc
Dariusz Filipiak <darekf77@gmail.com>
2019-10-13 03:24:15 +0200

    ad

--- projects/container/site/ss-common-logic/custom/src/apps/project/project-ui/tnp-project/menu-changer-tnp-project.ts
+++ projects/container/site/ss-common-logic/custom/src/apps/project/project-ui/tnp-project/menu-changer-tnp-project.ts
@@ -1,5 +1,5 @@
-import { TnpProjectTabIndex } from "./project-tab-index";
-import { PROJECT } from "../../PROJECT";
+import { TnpProjectTabIndex } from './project-tab-index';
+import { PROJECT } from '../../PROJECT';
 
 
 
@@ -11,22 +11,22 @@ export class MenuChangerTnpProject {
 
   }
 
-  constructor(
-    public project: PROJECT) {
+  // constructor(
+  //   public project: PROJECT) {
 
-    if (project.isStandaloneProject) {
-      this.currentStep = TnpProjectTabIndex.RELEASE;
-    } else {
-      this.project.proc
-      if (this.project.procStaticBuild.state === 'notStarted') {
-        this.currentStep = TnpProjectTabIndex.ENV;
-      } else {
-        if (this.project.procStaticBuild.state === '') {
+  //   if (project.isStandaloneProject) {
+  //     this.currentStep = TnpProjectTabIndex.RELEASE;
+  //   } else {
+  //     this.project.proc
+  //     if (this.project.procStaticBuild.state === 'notStarted') {
+  //       this.currentStep = TnpProjectTabIndex.ENV;
+  //     } else {
+  //       if (this.project.procStaticBuild.state === '') {
 
-        }
-      }
-    }
-  }
+  //       }
+  //     }
+  //   }
+  // }
 
 
 
