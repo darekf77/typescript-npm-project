1bb032d6f963a0e57e351adbb728471fea850930
Dariusz <darekf77@gmail.com>
2020-01-01 06:50:58 +0100

    asd

--- src/scripts/PROJECTS-DEVELOPMENT/GITHUB.backend.ts
+++ src/scripts/PROJECTS-DEVELOPMENT/GITHUB.backend.ts
@@ -11,9 +11,9 @@ const ADDRESS_GITHUB = os.userInfo().username === 'dfilipiak' ? ADDRESS_GITHUB_H
 const NPM_PROJCETS_LOCATION = path.resolve(path.join(Project.Tnp.location, '..'));
 const GITHUB_PROJECTS_NAMES = [
   'tsc-npm-project',
-  'tsc-helpers',
-  'tsc-models',
-  'tsc-db',
+  'tnp-helpers',
+  'tnp-models',
+  'tnp-db',
   'firedev',
   'firedev-projects',
   'ng2-rest',
