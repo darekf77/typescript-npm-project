016d5ebd73c0b4962990c229f315bf31f1044ec3
Dariusz Filipiak <dariusz.filipiak@igt.com>
2020-08-14 17:26:39 +0200

    a

--- scripts/configure-linux.sh
+++ scripts/configure-linux.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 9.4 && npm install -g npm
-npm install -g check-node-version npm-run@4.1.2 rimraf cpr renamer nodemon typescript@2.6.2
+npm install -g check-node-version npm-run@4.1.2 rimraf cpr renamer nodemon typescript@3.8.3
 npm link
 ssh-keygen -t rsa -b 4096 -C "darekf77@gmail.com" && cat ~/.ssh/id_rsa.pub
 read -n1 -r -p "Please add the ssh key above to your github ssh keys..." key
