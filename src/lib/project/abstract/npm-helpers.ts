f20f488de91ec26c9c68913370cba6018762c9f5
Dariusz Filipiak <darekf77@gmail.com>
2024-07-14 10:20:32 +0200

    $$$ update $$$

--- src/lib/project/abstract/npm-helpers.ts
+++ src/lib/project/abstract/npm-helpers.ts
@@ -1,4 +1,16 @@
 import { BaseNpmHelpers } from 'tnp-helpers/src';
 import type { Project } from './project';
 
-export class NpmHelpers extends BaseNpmHelpers<Project> {}
+export class NpmHelpers extends BaseNpmHelpers<Project> {
+  project: Project;
+  async makeSureNodeModulesInstalled(options?: {
+    useYarn?: boolean;
+    force?: boolean;
+  }) {
+    if (this.project.__isStandaloneProject || this.project.__isContainer) {
+      // TODO @LAST
+      return;
+    }
+    await super.makeSureNodeModulesInstalled(options);
+  }
+}
