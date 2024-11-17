559900b604086dd8fc8b90812e2df0c47d5436bf
Dariusz Filipiak <darekf77@gmail.com>
2024-11-17 08:01:27 +0100

    release:  v18.0.14

--- src/lib/project/cli/cli-SERVICE.backend.ts
+++ src/lib/project/cli/cli-SERVICE.backend.ts
@@ -1,11 +1,13 @@
 import { _, child_process } from 'tnp-core/src';
-import { Helpers } from 'tnp-helpers/src';
+import { Helpers, UtilsTerminal } from 'tnp-helpers/src';
 import { CommandLineFeature } from 'tnp-helpers/src';
 import { Project } from '../abstract/project';
 import { BuildOptions } from '../../build-options';
 import { config } from 'tnp-config/src';
 import { Taon, BaseContext } from 'taon/src';
 import { fse, chalk } from 'tnp-core/src';
+import { UtilsProcess } from 'tnp-core/src';
+import { CLASS } from 'typescript-class-helpers/src';
 
 @Taon.Entity({
   className: 'Port',
@@ -121,9 +123,11 @@ class $Service extends CommandLineFeature<{}, Project> {
     const ref = await PortsContext.initialize({
       overrideRemoteHost: `http://localhost:${START_PORT}`,
     });
-    const portControllerInstance = ref.getInstanceBy(PortsController);
-    const info = await portControllerInstance.info().received;
-    console.log(`Info from service: "${info.body.text}"`);
+    this._bashOrShellInfo();
+    // const portControllerInstance = ref.getInstanceBy(PortsController);
+    // const info = await portControllerInstance.info().received;
+    // console.log(`Info from service: "${info.body.text}"`);
+    this._infoScreen();
   }
 
   /**
@@ -143,17 +147,85 @@ class $Service extends CommandLineFeature<{}, Project> {
    * start if not started detached process
    */
   start() {
-    Helpers.run(`${config.frameworkName} service:startCommand`, {
-      biggerBuffer: true,
-    }).async(true);
+    Helpers.run(
+      `${config.frameworkName} ${CLASS.getName($Service)}:${'startCommand' as keyof $Service}`,
+      {
+        biggerBuffer: true,
+      },
+    ).async(true);
+    this._bashOrShellInfo();
     Helpers.info(`Detached service started...`);
     this._exit(0);
+  }
+
+  async _infoScreen() {
+    while (true) {
+      Helpers.clearConsole();
+      Helpers.info(
+        `Service started on port http://localhost${chalk.bold(START_PORT.toString())}`,
+      );
+      const choices = {
+        openBrowser: {
+          name: 'Open browser with service app',
+        },
+        showAllProcesses: {
+          name: 'Show all processes',
+        },
+        showLogo: {
+          name: 'Show logo',
+        },
+        exit: {
+          name: `Shut down ${chalk.bold(_.upperFirst(config.frameworkName) + ' service')}`,
+        },
+      };
+      const choice = await UtilsTerminal.select<keyof typeof choices>({
+        choices,
+        question: 'Choose action',
+      });
+      if (choice === 'exit') {
+        this._exit(0);
+      }
+      switch (choice) {
+        case 'exit':
+          this._exit(0);
+
+        case 'showLogo':
+          Helpers.clearConsole();
+          console.log(this.logo);
+          Helpers.pressKeyAndContinue();
+          continue;
+      }
+    }
+  }
+
+  get logo() {
+    return `               YY
+      YXY    ZXXXX    ZYXY
+     ZXXXXYZYXXXXXXXYXXXXY
+     XXXXXWTTTVXWUTTUXXXXX
+    ZXXXXVTTTTRRRSTTTUWXXXY
+    YXXXUTTTSRRRRRRTTTTVXXX
+    XXXUTTTTTRRRRRRTTTTTVXXX
+   ZXXXXVTTTTRRRRRTTTTTWXXXX
+   YXXXXXWUTTTRRRRTTTUXXXXXX
+      YXXXXUTTRRRSTTVXXXXY
+        ZXXXVTTRRTTWXXY
+           ZXWTRSUXY
+              YTVZ    `;
+  }
+
+  private _bashOrShellInfo() {
+    const currentBash = UtilsProcess.getBashOrShellName();
+    Helpers.logInfo(` current bash/shell: ${currentBash}`);
   }
 
   /**
    * stop if started
    */
-  kill() {}
+  kill() {
+    Helpers.killOnPort(START_PORT);
+    this._exit();
+  }
 
   /**
    * kill detached process and start again
